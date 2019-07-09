1ea18b34f90a940bdf5d557a8129a2e8bd47991f
rivedroz23 <river.droz@gmail.com>
2019-07-09 16:14:36 -0700

    assessment done

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models');
+
+
 
 const app = express();
 
@@ -8,7 +11,32 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
+
+
 // WRITE YOUR ROUTES HERE /////////////////////
+
+app.get('/', function(req,res) {
+    db.widget.findAll().then(function(widgets) {
+        res.render('index', {widgets});
+    });
+});
+
+app.post('/', function(req,res) {
+    db.widget.create({
+       description: req.body.description,
+       quantity: req.body.quantity
+    }).then(function(widget) {
+        res.redirect('/');
+    })
+})
+
+app.delete('/:id', function(req,res) {
+    db.widget.destroy({
+        where: {id: parseInt(req.params.id)}
+    }).then(function(data) {
+        res.redirect('/');
+    });  
+ });
 
 
 
